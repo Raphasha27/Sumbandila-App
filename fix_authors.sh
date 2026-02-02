00c0951fa7c7cb7c5750a77b31a313b3df8aba49
Raphasha27 <raphasha27@gmail.com>
2026-02-02 11:25:10 +0200

    chore: cleanup before history rewrite

---
+++ fix_authors.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL_OKO="okomwezo@gmail.com"
+OLD_EMAIL_BOT="49699333+dependabot[bot]@users.noreply.github.com"
+CORRECT_NAME="Raphasha27"
+CORRECT_EMAIL="raphasha27@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL_OKO" ] || [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL_BOT" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL_OKO" ] || [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL_BOT" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags -f
