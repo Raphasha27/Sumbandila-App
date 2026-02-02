35bb38f8ab662e9b727eb0c6229e7328c1622c96
Raphasha27 <raphasha27@gmail.com>
2026-02-02 11:54:51 +0200

    chore: fix vulnerabilities and add codeql scanning

--- fix_authors.sh
+++ fix_authors.sh
@@ -5,11 +5,29 @@ CORRECT_NAME="Raphasha27"
 CORRECT_EMAIL="raphasha27@gmail.com"
 
 # Re-attribute everything that isnt you
-if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ] || [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
+case "$GIT_AUTHOR_NAME" in
+    *"dependabot"*|*"Oko"*)
+        export GIT_AUTHOR_NAME="$CORRECT_NAME"
+        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        ;;
+esac
+
+case "$GIT_COMMITTER_NAME" in
+    *"dependabot"*|*"Oko"*)
+        export GIT_COMMITTER_NAME="$CORRECT_NAME"
+        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+        ;;
+esac
+
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ] && [ "$GIT_AUTHOR_EMAIL" != "raphashakoketso69@gmail.com" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ] && [ "$GIT_COMMITTER_EMAIL" != "raphashakoketso69@gmail.com" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
