8848f8180b3c39f75fc5261447e637158c15e656
Raphasha27 <raphashakoketso69@gmail.com>
2026-04-16 03:15:20 +0200

    chore: major repository stabilization and cleanup for institutional funding

--- check_prs.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-url = 'https://api.github.com/repos/Raphasha27/Sumbandila-App/pulls?state=open'
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    data = json.loads(response.read().decode())
-    for pr in data:
-        print(f"#{pr['number']}: {pr['title']}")
